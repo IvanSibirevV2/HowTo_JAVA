d80dedd17d85807d668e72413acc422a730678b4
йцу <>
2024-10-10 14:57:13 +0300

    AutoCommit_10 октября 2024 г. 14:57:13_SibNout2023

--- ExampleS/E_000_HelloWorldClass.java
+++ ExampleS/E_000_HelloWorldClass.java
@@ -1,14 +1,8 @@
-//package HelloWorld;
-import java.util.Scanner;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+//Название файла //E_000_HelloWorldClass.Java//package HelloWorld;
+import java.util.Scanner; import java.nio.file.Path; import java.nio.file.Paths; import java.io.File; import java.io.BufferedReader; import java.io.IOException; import java.io.InputStreamReader;
+///////////////////////////////////////////////////////////////////////////////////////
 // Для работы формата примера название класса должно совпадать с названием примера.
 public class E_000_HelloWorldClass {
-
 	public static void main(String[] args) throws IOException {
 		Class<?> myClass = E_000_HelloWorldClass.class;
 		String qwe1 = System.getProperty("user.dir");
@@ -24,8 +18,12 @@ public class E_000_HelloWorldClass {
 			BufferedReader reader2 = new BufferedReader( new InputStreamReader(process.getInputStream())); String line;
 			while ((line = reader2.readLine()) != null) {System.out.println(line);}reader2.close(); System.exit(0);
 		}else{}
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 		//Программыный код писать сюда.
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld".getClass().getName());
 		//Ожидание ввода 
 		Scanner reader = new Scanner(System.in); int n = reader.nextInt(); reader.close();
 	}
---
+++ ExampleS/HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,41 @@
+package HelloWorld;
+
+import java.util.Scanner; import java.nio.file.Path; import java.nio.file.Paths; import java.io.File; import java.io.BufferedReader; import java.io.IOException; import java.io.InputStreamReader;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Программыный код писать сюда.
+		System.out.println("HelloWorld");
+		//Ожидание ввода 
+		if(!false)
+		{
+			boolean isActive = false;
+			boolean isAlive = true;
+			System.out.println("boolean isActive = "+ isActive);
+			String sdas="dada";
+			System.out.println("isActive.getClass().getName() = ");
+		
+			boolean _flagA = false; System.out.println("boolean _flagA = "+ _flagA +";");
+			boolean _flagB = true; System.out.println("boolean _flagB = "+ _flagB +";");
+			System.out.println("///////////////////////////////////////////////////////");
+			byte _byteMin = -128; System.out.println("byte _byteMin = "+ _byteMin +";");
+			byte _byte = 8; System.out.println("byte _byte = "+ _byte +";");
+			byte _byteMax = 127; System.out.println("byte _byteMax = "+ _byteMax +";");
+			//System.out.println("Short.MIN_VALUE = "+ Short.SIZE)
+			System.out.println("///////////////////////////////////////////////////////");
+			short _shortMin = -32768; System.out.println("short _shortMin = "+ _shortMin +";");
+			short _short = 8; System.out.println("short _short = "+ _short +";");
+			short _shortMax = 32767; System.out.println("short _shortMax = "+ _shortMax +";");
+			System.out.println("Int .class.getTypeName() = "+ long.class.getTypeName());
+			long a = 5;
+			System.out.println("Float.class.getTypeName() = "+ Float.class.getTypeName());
+			
+		
+		}
+		else if(false){}
+		Scanner reader = new Scanner(System.in); int n = reader.nextInt(); reader.close();
+	}
+
+}
